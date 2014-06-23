52da37d3ea53c793e6860dd931b4ff81a9e8ac12
Andrew <mendeleev.ua@gmail.com>
2014-06-23 19:49:07 +0300

    adding comments to controller

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,24 +1,29 @@
 var ctrl = angular.module('Controllers', ['testControllers']);
 
+//task controller
 ctrl.controller('taskCtrl', ['$scope', '$routeParams', '$http', 
 	function($scope, $routeParams, $http) {
 		$scope.task = null;
 		$scope.answers = {};
+
+		//switch on/off task validation
 		$scope.showValidation = false;
+
+		//loading task data
 		$http.get('app/data/' + $routeParams.testId + '.json').success(function(data) {
 			$scope.task = data;
-			if(data.template) { 
-				$scope.templateUrl = "app/views/" + data.template;
-			} else {
-				$scope.templateUrl = "app/views/tmpl.html";
-			}		
+
+			//loads individual template if task has
+			//if not, loads the default template
+			$scope.templateUrl = data.template ? "app/views/" + data.template : "app/views/default.html";		
 		});
 
-
+		//returns boolean on matching answers
 		var matchAnswer = function(answer1, answer2) {
 			return answer1.toLowerCase() === answer2.toLowerCase();
 		};
 
+		//check if the user answer is valid
 		var validateAnswer = function(index) {
 			var answer1 = $scope.task.options[index].answer;
 			var answer2 = $scope.answers[index];
@@ -30,43 +35,35 @@ ctrl.controller('taskCtrl', ['$scope', '$routeParams', '$http',
 			}
 		};
 
+		//show correct sign if the answer is valid
 		$scope.showCorrectMessage = function(index) {
 			return validateAnswer(index);
 		};
 
+		//show incorrect message if the answer isn't valid
 		$scope.showIncorrectMessage = function(index) {
 			if(!validateAnswer(index)) return true;
 		}; 	
 
+		// show/hide answers validation
 		$scope.checkAnswers = function() {
-
-			if($scope.showValidation) { 
-				$scope.showValidation = false; 
-			} else {
-				$scope.showValidation = true;
-			}
-
-			for(var i = 0; i < $scope.task.options.length; i++) {
-				if($scope.answers[i]) {
-					var match = matchAnswer($scope.task.options[i].answer, $scope.answers[i]);
-					$scope.answers[i].isCorrect = match;
-				} else {
-					console.log($scope.answers[i]);
-				}				
-			}
+			$scope.showValidation = $scope.showValidation ? false : true;
 		};
 	}
 ]);
 
+//navigation controller
 ctrl.controller('navigationCtrl', ['$scope', '$http', '$location', 
 	function($scope, $http, $location) {
 		$scope.navigation = navOptions;	
 
+		//getting test id from the url
 		var getPathId = function() {
 			var testId = $location.url();
 			return parseInt(testId.substring(1));
 		};
 
+		//making navigation list
 		var getTestList = function() {
 			var list = [];
 			angular.forEach($scope.navigation, function(exercise) {
@@ -77,6 +74,7 @@ ctrl.controller('navigationCtrl', ['$scope', '$http', '$location',
 			return list;
 		};
 
+		//getting the current position
 		var getCurrentPos = function(list) {
 			var testId = getPathId();
 			var currentPos = 0;
@@ -87,16 +85,23 @@ ctrl.controller('navigationCtrl', ['$scope', '$http', '$location',
 					currentPos++;
 				}
 			}
+			
 			return currentPos;
 		};	
 
+		//creating a new list
 		var testList = getTestList();
+
+		//getting current position
 		var current = getCurrentPos(testList);
 
+		//set current position
+		//when click on the navigation element
 		$scope.setCurrentPos = function(pos) {
 			current = pos;
 		};	
 
+		//go to the next task when click next
 		$scope.nextTask = function() {
 			current = getCurrentPos(testList);
 			if(testList.length -1 > current) {
@@ -105,6 +110,7 @@ ctrl.controller('navigationCtrl', ['$scope', '$http', '$location',
 			}
 		};
 
+		//go to the previous task when click prev
 		$scope.prevTask = function() {				
 			current = getCurrentPos(testList);
 			if(current > 0) {
